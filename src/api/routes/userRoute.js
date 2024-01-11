6e0e99479a491841233fb0b444f25104956048bb
endricklosange <endricklosange@gmail.com>
2024-01-11 14:28:47 +0100

    not done

--- src/api/routes/userRoute.js
+++ src/api/routes/userRoute.js
@@ -1,3 +1,67 @@
+/**
+* @swagger
+* /user/register:
+*   post:
+*     summary: Enregistre un nouvel utilisateur
+*     tags: [Utilisateurs]
+*     requestBody:
+*       required: true
+*       content:
+*         application/json:
+*           schema:
+*             type: object
+*             required:
+*               - name
+*               - email
+*               - password
+*             properties:
+*               name:
+*                 type: string
+*               role:
+*                 type: string
+*                 enum: [user, admin]
+*                 default: user
+*               email:
+*                 type: string
+*                 format: email
+*               password:
+*                 type: string
+*     responses:
+*       200:
+*         description: Utilisateur enregistré avec succès
+*       400:
+*         description: Données invalides
+*       500:
+*         description: Erreur serveur
+* @swagger
+* /user/login:
+*   post:
+*     summary: Connecte un utilisateur
+*     tags: [Utilisateurs]
+*     requestBody:
+*       required: true
+*       content:
+*         application/json:
+*           schema:
+*             type: object
+*             required:
+*               - email
+*               - password
+*             properties:
+*               email:
+*                 type: string
+*                 format: email
+*               password:
+*                 type: string
+*     responses:
+*       200:
+*         description: Connexion réussie
+*       401:
+*         description: Authentification échouée
+*       500:
+*         description: Erreur serveur
+*/
+
 module.exports = (server) => {
     const userController = require("../controllers/userController");
     
--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ const hostname = '0.0.0.0';
 const port = 3000;
 const server = express();
 const mongoose = require('mongoose');
+const swaggerJsdoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
 
 mongoose.connect('mongodb://mongo/apinode');
 
@@ -18,7 +20,39 @@ commentRoute(server);
 
 const userRoute = require("./api/routes/userRoute");
 userRoute(server);
-
+const options = {
+    definition: {
+      openapi: "3.1.0",
+      info: {
+        title: "LogRocket Express API with Swagger",
+        version: "0.1.0",
+        description:
+          "This is a simple CRUD API application made with Express and documented with Swagger",
+        license: {
+          name: "MIT",
+          url: "https://spdx.org/licenses/MIT.html",
+        },
+        contact: {
+          name: "LogRocket",
+          url: "https://logrocket.com",
+          email: "info@email.com",
+        },
+      },
+      servers: [
+        {
+          url: "http://localhost:3000",
+        },
+      ],
+    },
+    apis: ["./api/routes/*.js"],
+  };
+  
+  const specs = swaggerJsdoc(options);
+  server.use(
+    "/docs",
+    swaggerUi.serve,
+    swaggerUi.setup(specs)
+  );
 server.listen(port, hostname, () => {
     console.log(`Example app listening on ${hostname} port ${port}`)
 });
